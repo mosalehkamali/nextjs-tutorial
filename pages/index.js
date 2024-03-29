b7b258105269668615ef799072a761662e98a694
mosalehkamali <mosalehkamali@gmail.com>
2024-03-29 18:10:23 +0430

    Handle-router

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
+import Link from "next/link";
+import db from "../data/db.json"
 
 export default function Home() {
   return (
     <>
-    <h1 className={styles.title}>Next-js tutorial</h1>
+      <h1 className="title">Next-js tutorial</h1>
+      <ul>
+        {
+          db.seasons.map(
+            season =>
+              <li key={season.id} >
+                <Link href={season.path}>{season.title}</Link>
+              </li>
+          )
+        }
+      </ul>
     </>
   );
 }
