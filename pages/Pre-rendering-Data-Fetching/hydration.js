fc07e349f887199bca636d2f39721a3be273af54
mosalehkamali <mosalehkamali@gmail.com>
2024-03-30 20:07:46 +0430

    create-hydration-page

--- pages/Pre-rendering-Data-Fetching/hydration.js
+++ pages/Pre-rendering-Data-Fetching/hydration.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import TopicHeader from '@/components/module/TopicHeader'
+import Link from 'next/link'
 
 function hydration() {
 
   return (
     <>
       <TopicHeader></TopicHeader>
+      <div className="topic-container">
+        <p className='topic-text'>
+          در ری اکت زمانی که ریکوئست به سرور فرستاده میشود ری اکت یک فایل  HTML خالی برای کلاینت ارسال میکند و اچ تی ام ال تمپلیت و کد های جی اس بعد از  JS load ایجاد میشوند اما در نکست سرور اچ تی م ال تمپلیت را به صورت استاتیک برای کلاینت ارسال میکند و فقط کد های جی اسی بعد از JS load فعال میشوند به این عمل hydration میگویند
+        </p>
+        <Link className='topic-link' href="https://nextjs.org/docs/messages/react-hydration-error">Next.js document about this topic</Link>
+      </div>
     </>
   )
 }
