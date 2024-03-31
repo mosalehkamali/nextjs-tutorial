60e38c0dd4a2e81cb0d7542ff5d3dcae743ec895
mosalehkamali <mosalehkamali@gmail.com>
2024-03-31 15:59:25 +0430

    getStaticPropsWithoutData

--- pages/Pre-rendering-Data-Fetching/getStaticPropsWithoutData.js
+++ pages/Pre-rendering-Data-Fetching/getStaticPropsWithoutData.js
@@ -1,11 +1,59 @@
 import React from 'react'
+import TopicHeader from '@/components/module/TopicHeader'
+import Link from 'next/link'
 
-function getStaticPropsWithoutData() {
+function getStaticPropsWithoutData({ textContent, example }) {
+
+    return (
+        <>
+            <TopicHeader></TopicHeader>
+            <p className='topic-text'> {textContent} </p>
+            <Link className='topic-link' href="https://nextjs.org/docs/pages/building-your-application/rendering/static-site-generation#static-generation-without-data">Next.js document about this topic</Link>
+            <pre className="code-example"> {example} </pre>
+        </>
+    )
+}
+
+export default getStaticPropsWithoutData
+
+export async function getStaticProps() {
+
+    console.log('getStaticProps runing') // این کد در سمت سرور اجرا میشود و در بروزر قابل دیدن نیست
+
+    return { // این پراپرتی ها به کامپوننت ریترن میشوند
+        props: {
+            textContent: `تابع getStaticProps در سمت سرور برای بک اند اجرا میشود 
+          ولی پراپرتی که ریترن میکند برای همان کامپوننت یا روت خودش قابل استفاده است
+          getStaticProps یک تابع async  می‌باشد. وبه شکل  زیر استفاده میشود `,
+            example: `
+import React from 'react'
+import Link from 'next/link'
+
+function getStaticPropsWithoutData({ textContent, example }) {
   return (
-    <div>
-      
-    </div>
+    <>
+     <p className='topic-text'> {textContent} </p>
+
+     <pre className="code-example"> {example} </pre>
+    </>
   )
 }
 
 export default getStaticPropsWithoutData
+
+export async function getStaticProps(){
+    
+    console.log('getStaticProps runing') // این کد در سمت سرور اجرا میشود و در بروزر قابل دیدن نیست
+
+    return { // این پراپرتی ها به کامپوننت ریترن میشوند
+        props:{
+          textContent  : 'تابع getStaticProps در سمت سرور برای بک
+           اند اجرا میشود ولی پراپرتی که ریترن میکند برای همان کامپوننت یا روت خودش قابل استفاده است
+          getStaticProps یک تابع async  می‌باشد. وبه شکل  زیر استفاده میشود',
+          example : 'کل این کد در تگ pre قرار میگیرد'
+        },
+    }
+}`
+        },
+    }
+}
