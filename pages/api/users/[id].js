da6964a3569a89b622ee09af865ec7bac90259fa
mosalehkamali <mosalehkamali@gmail.com>
2024-04-18 21:41:25 +0430

    GetDocumentData

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,7 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import connectToDB from '@/utils/users';
+import usersModel from '@/models/user';
 
-export default function handle(req, res) {
+export default async function handle(req, res) {
+    connectToDB()
     if (req.method === "DELETE") { //متد را چک میکنیم
 
         const query = req.query.id //آی دی کوئری ارسال شده را ذخیره میکنیم
@@ -37,19 +40,19 @@ export default function handle(req, res) {
         }
     } else if (req.method === "PUT") {
         const { name, email } = req.body //تغییراتی که باید انجام شود را دریافت میکنیم
-        
+
         const query = req.query.id //آی دی کوئری ارسال شده را ذخیره میکنیم
-        
+
         //آدرس فایل دیتابیس را ذخیره میکنیم
         const filePath = path.join(process.cwd(), '/data', 'users.json');
         //دیتا را از ادرس ایجاد شده میخوانیم
         const users = fs.readFileSync(filePath);
         //دیتا را به صورت آبجکت در آورده و ذخیره میکنیم
         const parsedUsers = JSON.parse(users).users;
-        
+
         //چک میکنیم که دیتای خواسته شده وجود دارد یا نه
         const isUser = parsedUsers.some(user => String(user.id) === String(query));
-        
+
         //اگر دیتا موجود بود
         if (isUser) {
             parsedUsers.some(user => {
@@ -67,12 +70,19 @@ export default function handle(req, res) {
                 res.status(403).json(err)
             } else {
                 //اگر خطا نداشت پیغام موفقیت حذف را ارسال میکنیم
-                res.json({message:"Data edited successfuly"})
+                res.json({ message: "Data edited successfuly" })
             }
         } else {
             //اگر دیتای خواسته شده وجود نداشت خطا را ارسال میکنیم
             res.status(404).json({ message: "User not found" });
         }
+    } else if (req.method === "GET") {
+        const id = req.query.id
+
+        const user = await usersModel.find({ _id: id })
+
+        res.json(user)
+
     } else {
         //اگر متد دلیت نبود عدم ساپرت متد را  ارسال میکنیم
         res.status(505).json({ message: "Method not supported" });
