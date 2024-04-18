c0fae6ddcd1c62dcca5eb0e1ad23437d6bad02d1
mosalehkamali <mosalehkamali@gmail.com>
2024-04-18 22:23:47 +0430

    UpdateDocument

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -23,41 +23,17 @@ export default async function handle(req, res) {
     } else if (req.method === "PUT") {
         const { name, email } = req.body //تغییراتی که باید انجام شود را دریافت میکنیم
 
-        const query = req.query.id //آی دی کوئری ارسال شده را ذخیره میکنیم
+        const id = req.query.id //آی دی کوئری ارسال شده را ذخیره میکنیم
 
-        //آدرس فایل دیتابیس را ذخیره میکنیم
-        const filePath = path.join(process.cwd(), '/data', 'users.json');
-        //دیتا را از ادرس ایجاد شده میخوانیم
-        const users = fs.readFileSync(filePath);
-        //دیتا را به صورت آبجکت در آورده و ذخیره میکنیم
-        const parsedUsers = JSON.parse(users).users;
+        const updatedUser= await usersModel.findOneAndUpdate({_id : id}, {name ,email})
 
-        //چک میکنیم که دیتای خواسته شده وجود دارد یا نه
-        const isUser = parsedUsers.some(user => String(user.id) === String(query));
-
-        //اگر دیتا موجود بود
-        if (isUser) {
-            parsedUsers.some(user => {
-                if (String(user.id === String(query))) { //دیتای مورد نظر را عوض میکنیم
-                    user.name = name
-                    user.email = email
-                    return true
-                } else return false
-            })
-            //دیتای ادیت شده را به دیتا بیس میفرستیم
-            const err = fs.writeFileSync(filePath, JSON.stringify({ "users": parsedUsers }));
-
-            if (err) {
-                // اگر با خطا مواجه شدیم خطا را ارسال میکنیم
-                res.status(403).json(err)
-            } else {
+        if (updatedUser) {
                 //اگر خطا نداشت پیغام موفقیت حذف را ارسال میکنیم
                 res.json({ message: "Data edited successfuly" })
+            } else {
+                // اگر با خطا مواجه شدیم خطا را ارسال میکنیم
+                res.status(404).json({ message: "User not found" });
             }
-        } else {
-            //اگر دیتای خواسته شده وجود نداشت خطا را ارسال میکنیم
-            res.status(404).json({ message: "User not found" });
-        }
     } else if (req.method === "GET") {
         const id = req.query.id
 
