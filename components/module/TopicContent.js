07e1fc68e89fe83c736c0d34b711da533702d65e
mosalehkamali <mosalehkamali@gmail.com>
2024-05-06 19:43:12 +0430

    imageComponentUsing Update

--- components/module/TopicContent.js
+++ components/module/TopicContent.js
@@ -3,12 +3,20 @@ import React from 'react'
 import CodeBox from './CodeBox'
 import TopicHeader from './TopicHeader'
 
-function TopicContent({ text, link, code }) {
+function TopicContent({ text, list, link, code }) {
     return (
         <>
             <TopicHeader></TopicHeader>
+
             <p className="topic-text">{text}</p>
+
+            {list ? <ul>{list.map(item => (
+                <li className="topic-text" key={list.indexOf(item)}>{item}</li>
+            ))}
+            </ul> : null}
+
             {link ? <Link className='topic-link' href={link}>Documentation about this topic</Link> : null}
+
             {code ? <CodeBox code={code} /> : null}
         </>
     )
--- pages/ProNextConcept/imageComponentUsing.js
+++ pages/ProNextConcept/imageComponentUsing.js
@@ -7,6 +7,11 @@ function imageComponentUsing() {
         <>
             <TopicContent
                 text={"کامپوننت Image  را میتوانیم بجای تگ img استفاده کنیم این کامپوننت عکس ها ار تا ده برابر بهینه تر و کم حجم تر میکند و عکس افت کیفیت ندارد برای این کار کامپوننت Image رااز next/image ایمپورت میکنیم"}
+                list={[
+                    "کامپوننت Image عکسهای ما را cache میکند.",
+                    "فرمت عکس ها را به webp تغییر میدهد که با مرورگر سازگارتر است",
+                    "برای عکس ها قابلیت lazy loading را میدهد یعنی تا زمانی که کاربر عکس عکس را نمی بیند عکس را دانلود نمیکند."
+                ]}
                 link={"https://nextjs.org/docs/pages/api-reference/components/image"}
                 code={ ` <Image src={'/images/post01.png'} width={850} height={400} alt={'course'}></Image>`}
             ></TopicContent>
