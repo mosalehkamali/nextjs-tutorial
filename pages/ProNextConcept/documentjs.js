72dbc50eaafb665be818749453286eb2a45d4d02
mosalehkamali <mosalehkamali@gmail.com>
2024-05-09 16:49:00 +0330

    documentjs

---
+++ pages/ProNextConcept/documentjs.js
@@ -0,0 +1,19 @@
+import TopicContent from "@/components/module/TopicContent";
+import React from "react";
+
+function documentjs() {
+  return (
+    <>
+      <TopicContent
+        text={
+          "فایل _document.js اچ تی ام ال اصلی و گلوبال برنامه را اجرا میکند در سمت سرور و همه همه صفحات درون این فایل اجرا میشوند"
+        }
+        link={
+          "https://nextjs.org/docs/pages/building-your-application/routing/custom-document"
+        }
+      ></TopicContent>
+    </>
+  );
+}
+
+export default documentjs;
