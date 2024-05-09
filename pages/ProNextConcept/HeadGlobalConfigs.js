4c1987e3527f6be5ad593b5a49cb736f610c35eb
mosalehkamali <mosalehkamali@gmail.com>
2024-05-09 12:20:33 +0330

    HeadGlobalConfigs

---
+++ pages/ProNextConcept/HeadGlobalConfigs.js
@@ -0,0 +1,16 @@
+import TopicContent from '@/components/module/TopicContent'
+import React from 'react'
+
+function HeadGlobalConfigs() {
+  return (
+    <>
+     <TopicContent
+     text={"اگر تگ هدی در همه صفحات یکسان باشد آن را در کامپوننت Head فایل _app.js  مینویسیم تا برای همه صفحات اعمال شود"}
+     link={"https://nextjs.org/docs/pages/api-reference/components/head"}
+     code={""}
+     ></TopicContent> 
+    </>
+  )
+}
+
+export default HeadGlobalConfigs
