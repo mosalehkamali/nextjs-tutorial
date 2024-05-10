9260e77460916146977259a5c6dd019203b9bac2
mosalehkamali <mosalehkamali@gmail.com>
2024-05-10 22:27:00 +0330

    RouteGroup

---
+++ pages/Next13/RouteGroup.js
@@ -0,0 +1,15 @@
+import TopicContent from '@/components/module/TopicContent'
+import React from 'react'
+
+function RouteGroup() {
+  return (
+    <>
+     <TopicContent
+     text={'در next13 قابلیتی اضافه شده به نام route group که میتوانیم روت هایی که به هم مربوط هستند را درون یک پوشه قرار بدهیم که به شکل (groupName) نام گذاری میشود و فقط برای خوانایی بهتر پروژه است و در روت بندی پروژه هیچ تاثیری ندارد'}
+     link={'https://nextjs.org/docs/app/building-your-application/routing'}
+     ></TopicContent> 
+    </>
+  )
+}
+
+export default RouteGroup
