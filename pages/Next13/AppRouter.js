3ba57543671b5e37ab8483a277b8a934863b2b38
mosalehkamali <mosalehkamali@gmail.com>
2024-05-10 20:37:20 +0330

    AppRouter Update

--- pages/Next13/AppRouter.js
+++ pages/Next13/AppRouter.js
@@ -6,6 +6,7 @@ function AppRouter() {
     <>
      <TopicContent
      text={'در نسخه next 13 قابلیت App Router اضافه شده برای روت بندی پروژه روت ها را در پوشه app بجای پوشه pages میسازیم و باید نام فایل ها بجای index , page  باشد'}
+     link={'https://nextjs.org/docs/app/building-your-application/routing#the-app-router'}
      ></TopicContent> 
     </>
   )
