e031749dcafe19c8fa8be3448710f665244e5a02
mosalehkamali <mosalehkamali@gmail.com>
2024-05-11 10:09:25 +0330

    ServerAndClientComponents

---
+++ pages/Next13/ServerAndClientComponents.js
@@ -0,0 +1,15 @@
+import TopicContent from '@/components/module/TopicContent'
+import React from 'react'
+
+function ServerAndClientComponents() {
+  return (
+    <>
+      <TopicContent
+      text={'یس ما در نکست 13 همه کامپوننت هایمان در سرور رندر میشوند پس نمیتوانیم از متدهای ری اکتی فرانت اند در این کامپوننت ها استفاده کنیم '}
+      link={'https://nextjs.org/docs/app/building-your-application/rendering/server-components'}
+      ></TopicContent>
+    </>
+  )
+}
+
+export default ServerAndClientComponents
