32570f4aaf1268d5163b08b8ee865e0f78af1230
mosalehkamali <mosalehkamali@gmail.com>
2024-05-11 23:38:20 +0330

    KnowingCeche

---
+++ pages/Next13/KnowingCeche.js
@@ -0,0 +1,15 @@
+import TopicContent from '@/components/module/TopicContent'
+import React from 'react'
+
+function KnowingCeche() {
+  return (
+    <>
+      <TopicContent
+      text={'زمانی که به فچ کردن دیتا در چند روت مختلف داریم ما کد فچ را چندین بار مینویسیم ولی نکست دیتا را فقط یک بار فچ میکند و دیتا را کش  و ذخیره میکند دفعات دیگر کد فچ را اجرا نمیکند و از دیتای کش شده ااستفاده میکند'}
+      link={'https://nextjs.org/docs/app/building-your-application/caching'}
+      ></TopicContent>
+    </>
+  )
+}
+
+export default KnowingCeche
