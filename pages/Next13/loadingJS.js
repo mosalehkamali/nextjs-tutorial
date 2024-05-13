3389f1b21c73ea703f4ba3f5802873471d0a4e7d
mosalehkamali <mosalehkamali@gmail.com>
2024-05-13 10:57:26 +0330

    loadingJS Update

--- pages/Next13/loadingJS.js
+++ pages/Next13/loadingJS.js
@@ -6,7 +6,7 @@ function loadingJS() {
     <>
       <TopicContent
         text={
-          "برای نمایش یک لودینگ تا زمانی که صفحه لود شود میتوانیم یک فایل به نام loading.js ایجاد کنیم و در  آن کامپوننت لودینگ را قرار بدهیم نکست جی اس این کامپوننت را به عنوان fallback برایSuspense قرار میدهد"
+          "برای نمایش یک لودینگ تا زمانی که صفحه لود شود میتوانیم یک فایل به نام loading.js ایجاد کنیم و در  آن کامپوننت لودینگ را قرار بدهیم نکست جی اس این کامپوننت را به عنوان fallback برایSuspense قرار میدهد. باید توجه داشته باشید که loading  به دور تا دور page فالبک میشود نه دور layout یعنی برای فایل های layout همسطح خودش اجرا نمیشود"
         }
         link={
           "https://nextjs.org/docs/app/building-your-application/routing/loading-ui-and-streaming"
